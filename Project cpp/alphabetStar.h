06e7e7efd065636843c8face7e0a7d2f78eb66cd
Bittu Sharma <144942287+BittuSharma2@users.noreply.github.com>
2024-01-10 00:34:20 +0530

    Create alphabetStar.h

---
+++ Project cpp/alphabetStar.h
@@ -0,0 +1,547 @@
+
+void A(int i)
+{
+    switch(i)
+    {
+        case 1:
+            cout<<"  *    ";
+            break;
+        case 2:
+            cout<<" * *   ";
+            break;
+        case 3:
+            cout<<"*****  ";
+            break;
+        case 4:
+            cout<<"*   *  ";
+            break;
+        case 5:
+            cout<<"*   *  ";
+    }
+}
+
+void B(int i)
+{
+    switch(i)
+    {
+        case 1:
+            cout<<"*****  ";
+            break;
+        case 2:
+            cout<<"*   *  ";
+            break;
+        case 3:
+            cout<<"*****  ";
+            break;
+        case 4:
+            cout<<"*   *  ";
+            break;
+        case 5:
+            cout<<"*****  ";
+    }
+}
+
+void C(int i)
+{
+    switch(i)
+    {
+        case 1:
+            cout<<" ****  ";
+            break;
+        case 2:
+            cout<<"*      ";
+            break;
+        case 3:
+            cout<<"*      ";
+            break;
+        case 4:
+            cout<<"*      ";
+            break;
+        case 5:
+            cout<<" ****  ";
+    }
+}
+
+void D(int i)
+{
+    switch(i)
+    {
+        case 1:
+            cout<<"****   ";
+            break;
+        case 2:
+            cout<<"*   *  ";
+            break;
+        case 3:
+            cout<<"*   *  ";
+            break;
+        case 4:
+            cout<<"*   *  ";
+            break;
+        case 5:
+            cout<<"****   ";
+    }
+}
+
+void E(int i)
+{
+    switch(i)
+    {
+        case 1:
+            cout<<"*****  ";
+            break;
+        case 2:
+            cout<<"*      ";
+            break;
+        case 3:
+            cout<<"*****  ";
+            break;
+        case 4:
+            cout<<"*      ";
+            break;
+        case 5:
+            cout<<"*****  ";
+    }
+}
+
+void F(int i)
+{
+    switch(i)
+    {
+        case 1:
+            cout<<"*****  ";
+            break;
+        case 2:
+            cout<<"*      ";
+            break;
+        case 3:
+            cout<<"*****  ";
+            break;
+        case 4:
+            cout<<"*      ";
+            break;
+        case 5:
+            cout<<"*      ";
+    }
+}
+
+void G(int i)
+{
+    switch(i)
+    {
+        case 1:
+            cout<<"*****  ";
+            break;
+        case 2:
+            cout<<"*      ";
+            break;
+        case 3:
+            cout<<"* ***  ";
+            break;
+        case 4:
+            cout<<"* * *  ";
+            break;
+        case 5:
+            cout<<"*** *  ";
+    }
+}
+
+void H(int i)
+{
+    switch(i)
+    {
+        case 1:
+            cout<<"*   *  ";
+            break;
+        case 2:
+            cout<<"*   *  ";
+            break;
+        case 3:
+            cout<<"*****  ";
+            break;
+        case 4:
+            cout<<"*   *  ";
+            break;
+        case 5:
+            cout<<"*   *  ";
+    }
+}
+
+void I(int i)
+{
+    switch(i)
+    {
+        case 1:
+            cout<<"*****  ";
+            break;
+        case 2:
+            cout<<"  *    ";
+            break;
+        case 3:
+            cout<<"  *    ";
+            break;
+        case 4:
+            cout<<"  *    ";
+            break;
+        case 5:
+            cout<<"*****  ";
+    }
+}
+
+void J(int i)
+{
+    switch(i)
+    {
+        case 1:
+            cout<<"*****  ";
+            break;
+        case 2:
+            cout<<"   *   ";
+            break;
+        case 3:
+            cout<<"*  *   ";
+            break;
+        case 4:
+            cout<<"*  *   ";
+            break;
+        case 5:
+            cout<<"****   ";
+    }
+}
+
+void K(int i)
+{
+switch(i)
+    {
+        case 1:
+            cout<<"*   *  ";
+            break;
+        case 2:
+            cout<<"* *    ";
+            break;
+        case 3:
+            cout<<"*      ";
+            break;
+        case 4:
+            cout<<"* *    ";
+            break;
+        case 5:
+            cout<<"*   *  ";
+    }
+}
+
+void L(int i)
+{
+    switch(i)
+    {
+        case 1:
+            cout<<"*      ";
+            break;
+        case 2:
+            cout<<"*      ";
+            break;
+        case 3:
+            cout<<"*      ";
+            break;
+        case 4:
+            cout<<"*      ";
+            break;
+        case 5:
+            cout<<"*****  ";
+    }
+}
+
+void M(int i)
+{
+    switch(i)
+    {
+        case 1:
+            cout<<"*   *  ";
+            break;
+        case 2:
+            cout<<"* * *  ";
+            break;
+        case 3:
+            cout<<"*   *  ";
+            break;
+        case 4:
+            cout<<"*   *  ";
+            break;
+        case 5:
+            cout<<"*   *  ";
+    }
+}
+
+void N(int i)
+{
+    switch(i)
+    {
+        case 1:
+            cout<<"*   *  ";
+            break;
+        case 2:
+            cout<<"**  *  ";
+            break;
+        case 3:
+            cout<<"* * *  ";
+            break;
+        case 4:
+            cout<<"*  **  ";
+            break;
+        case 5:
+            cout<<"*   *  ";
+    }
+}
+
+void O(int i)
+{
+    switch(i)
+    {
+        case 1:
+            cout<<" ***   ";
+            break;
+        case 2:
+            cout<<"*   *  ";
+            break;
+        case 3:
+            cout<<"*   *  ";
+            break;
+        case 4:
+            cout<<"*   *  ";
+            break;
+        case 5:
+            cout<<" ***   ";
+    }
+}
+
+void P(int i)
+{
+    switch(i)
+    {
+        case 1:
+            cout<<"****   ";
+            break;
+        case 2:
+            cout<<"*   *  ";
+            break;
+        case 3:
+            cout<<"****   ";
+            break;
+        case 4:
+            cout<<"*      ";
+            break;
+        case 5:
+            cout<<"*      ";
+    }
+}
+
+void Q(int i)
+{
+    switch(i)
+    {
+        case 1:
+            cout<<" ***   ";
+            break;
+        case 2:
+            cout<<"*   *  ";
+            break;
+        case 3:
+            cout<<"* * *  ";
+            break;
+        case 4:
+            cout<<"*   *  ";
+            break;
+        case 5:
+            cout<<" *** * ";
+    }
+}
+
+void R(int i)
+{
+    switch(i)
+    {
+        case 1:
+            cout<<"*****  ";
+            break;
+        case 2:
+            cout<<"*   *  ";
+            break;
+        case 3:
+            cout<<"*****  ";
+            break;
+        case 4:
+            cout<<"* *    ";
+            break;
+        case 5:
+            cout<<"*   *  ";
+    }
+}
+
+void S(int i)
+{
+    switch(i)
+    {
+        case 1:
+            cout<<"*****  ";
+            break;
+        case 2:
+            cout<<"*      ";
+            break;
+        case 3:
+            cout<<"*****  ";
+            break;
+        case 4:
+            cout<<"    *";
+            break;
+        case 5:
+            cout<<"*****  ";
+    }
+}
+
+void T(int i)
+{
+    switch(i)
+    {
+        case 1:
+            cout<<"*****  ";
+            break;
+        case 2:
+            cout<<"  *    ";
+            break;
+        case 3:
+            cout<<"  *    ";
+            break;
+        case 4:
+            cout<<"  *    ";
+            break;
+        case 5:
+            cout<<"  *    ";
+    }
+}
+
+void U(int i)
+{
+    switch(i)
+    {
+        case 1:
+            cout<<"*   *  ";
+            break;
+        case 2:
+            cout<<"*   *  ";
+            break;
+        case 3:
+            cout<<"*   *  ";
+            break;
+        case 4:
+            cout<<"*   *  ";
+            break;
+        case 5:
+            cout<<"*****  ";
+    }
+}
+
+void V(int i)
+{
+    switch(i)
+    {
+        case 1:
+            cout<<"*   *  ";
+            break;
+        case 2:
+            cout<<"*   *  ";
+            break;
+        case 3:
+            cout<<"*   *  ";
+            break;
+        case 4:
+            cout<<" * *   ";
+            break;
+        case 5:
+            cout<<"  *    ";
+    }
+}
+
+void W(int i)
+{
+    switch(i)
+    {
+        case 1:
+            cout<<"*   *  ";
+            break;
+        case 2:
+            cout<<"*   *  ";
+            break;
+        case 3:
+            cout<<"*   *  ";
+            break;
+        case 4:
+            cout<<"* * *  ";
+            break;
+        case 5:
+            cout<<"*   *  ";
+    }
+}
+
+void X(int i)
+{
+    switch(i)
+    {
+        case 1:
+            cout<<"*   *  ";
+            break;
+        case 2:
+            cout<<" * *   ";
+            break;
+        case 3:
+            cout<<"  *    ";
+            break;
+        case 4:
+            cout<<" * *   ";
+            break;
+        case 5:
+            cout<<"*   *  ";
+    }
+}
+
+void Y(int i)
+{
+    switch(i)
+    {
+        case 1:
+            cout<<"*   *  ";
+            break;
+        case 2:
+            cout<<" *  *  ";
+            break;
+        case 3:
+            cout<<"   *   ";
+            break;
+        case 4:
+            cout<<"  *    ";
+            break;
+        case 5:
+            cout<<"*      ";
+    }
+}
+
+void Z(int i)
+{
+    switch(i)
+    {
+        case 1:
+            cout<<"*****  ";
+            break;
+        case 2:
+            cout<<"   *   ";
+            break;
+        case 3:
+            cout<<"  *    ";
+            break;
+        case 4:
+            cout<<" *     ";
+            break;
+        case 5:
+            cout<<"*****  ";
+    }
+}
+
